79d80fa27ad74ad10025d840197708c50c5086ac
vaelliott <victoria.elliott@phs.scot>
2024-04-08 14:57:36 +0100

    adding package

--- global.R
+++ global.R
@@ -3,6 +3,7 @@ library(bslib)
 library(phsstyles)
 library(shinyjs) # for useShinyjs() function expaning geography filters so parent/child logic 
 library(htmltools) # for landing page template to read
+library(purrr) # needed for sourcing modules with walk
 
 # modules ----------------------------------------------------------------------
 list.files("modules") |> 
