102c6c9701225b815f9c0c752859959cabae7b58
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-04-08 14:50:06 +0100

    source modules

--- global.R
+++ global.R
@@ -3,3 +3,7 @@ library(bslib)
 library(phsstyles)
 library(shinyjs) # for useShinyjs() function expaning geography filters so parent/child logic 
 library(htmltools) # for landing page template to read
+
+# modules ----------------------------------------------------------------------
+list.files("modules") |> 
+  map(~ source(paste0("modules/", .)))
