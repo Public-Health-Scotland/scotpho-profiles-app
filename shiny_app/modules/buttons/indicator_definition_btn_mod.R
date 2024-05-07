6b80fd454f830fd5099e8327ea91349e61e6b904
vaelliott <victoria.elliott@phs.scot>
2024-05-07 11:59:28 +0100

    module for definition module button

---
+++ shiny_app/modules/buttons/indicator_definition_btn_mod.R
@@ -0,0 +1,35 @@
+# modal dialog box opens and displays definition of selected indicator
+
+ 
+###UI
+
+indicator_definition_btn_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+      actionButton(ns("indicator_definition"), label = "Definition",icon= icon('info'))
+    )
+
+}
+
+
+####SERVER
+
+indicator_definition_btn_server <- function(id, selected_indicator) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+
+        observeEvent(input$indicator_definition, {
+        showModal(
+          modalDialog(
+            title = selected_indicator()
+          )
+        )
+      })
+
+      
+     })
+
+
+} #closer server
+
