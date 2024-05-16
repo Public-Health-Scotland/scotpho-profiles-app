27e9585942a9c24af2b12b599bbee8742525d22c
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-05-16 10:54:15 +0100

    add scotland shapefile

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -39,6 +39,9 @@ list.files("modules", full.names = TRUE, recursive = TRUE) |>
 # 3. Required datafiles ------------------------------------------------------------
 main_dataset <- read_parquet("data/optdata") # main dataset (to do: rename optdata file in data prep script)
 geo_lookup <- readRDS("data/geo_lookup.rds") # geography lookup
+geo_lookup <- setDT(geo_lookup)
+
+
 
 # shapefiles (for map) 
 ca_bound <- readRDS("data/CA_boundary.rds") # Council area
@@ -46,7 +49,7 @@ hb_bound <- readRDS("data/HB_boundary.rds") # Health board
 hscp_bound <- readRDS("data/HSCP_boundary.rds")# HSC Partnerships
 hscloc_bound <- readRDS("data/HSC_locality_boundary.rds") # HSC localities
 iz_bound <- readRDS("data/IZ_boundary.rds") # Intermediate zone
-
+scot_bound <- readRDS("data/scotland_bound.rds") # scotland
 # transform so in right format to join to main dataset 
 # this should maybe  be done in data prep instead so don't need to load sf package into the app - just leaflet?)
 ca_bound <- sf::st_as_sf(ca_bound)
@@ -54,6 +57,7 @@ hb_bound <- sf::st_as_sf(hb_bound)
 hscp_bound <- sf::st_as_sf(hscp_bound)
 hscloc_bound <- sf::st_as_sf(hscloc_bound)
 iz_bound <- sf::st_as_sf(iz_bound)
+scot_bound <- sf::st_as_sf(scot_bound)
 
 
 
