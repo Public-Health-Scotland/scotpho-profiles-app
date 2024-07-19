b1a21a54f68ea6cdd3183defc363a502967bb058
vaelliott <victoria.elliott@phs.scot>
2024-07-19 10:04:14 +0100

    start adapting app to accept pop groups file

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -50,6 +50,8 @@ simd_dataset <- read_parquet("data/deprivation_data") # dataset behind simd pane
 
 techdoc <- read_parquet("data/techdoc") # technical document
 
+popgroup_dataset <- read_parquet("data/popgroup_dataset") # dataset behind simd panel
+
 #temp data upload and simple wrangle
 ineq_splits_data <- readr::read_csv("data/88007_meeting_mvpa_im.csv") |>
   rename(areatype = geography,
