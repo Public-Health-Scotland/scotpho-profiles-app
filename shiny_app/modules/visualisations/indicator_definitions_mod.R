0fbc52e4682a9f8c47b00c6c369a44e3c8384304
Monica McGibbon <monica.mcgibbon@phs.scot>
2024-09-05 09:51:39 +0100

    fix typos

--- shiny_app/modules/visualisations/indicator_definitions_mod.R
+++ shiny_app/modules/visualisations/indicator_definitions_mod.R
@@ -69,7 +69,7 @@ definitions_tab_Server <- function(id) {
         if(input$profile_search != "All"){
           x <- x |>
             filter(if_any(contains("profile_domain"),
-                          ~ substr(.x, 1, 3) %in% profiles_list[[input$profile_choices]]))
+                          ~ substr(.x, 1, 3) %in% profiles_list[[input$profile_search]]))
         }
         
         x
