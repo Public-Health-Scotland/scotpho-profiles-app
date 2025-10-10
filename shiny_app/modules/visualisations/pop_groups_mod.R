f02ff4472cd384d055abee488882ac6b6eef4670
monicamcgibbon <monica.mcgibbon@phs.scot>
2025-10-10 13:37:38 +0000

    add CIs to popgroup data

--- shiny_app/modules/visualisations/pop_groups_mod.R
+++ shiny_app/modules/visualisations/pop_groups_mod.R
@@ -326,7 +326,7 @@ pop_groups_server <- function(id, dataset, geo_selections, selected_profile, roo
     output$pop_rank_table <- renderReactable({
       
       data <- pop_rank_data() |>
-        select(def_period, split_value, measure)
+        select(def_period, split_value, measure, upci, lowci)
       
       reactable(data = data,
                 defaultExpanded = TRUE,
@@ -335,7 +335,9 @@ pop_groups_server <- function(id, dataset, geo_selections, selected_profile, roo
                 columns = list(
                   def_period = colDef(name = "Time Period"),
                   split_value = colDef(name = "Population Group"),
-                  measure = colDef(name = "Measure")
+                  measure = colDef(name = "Measure"),
+                  upci = colDef(name = "Upper CI"),
+                  lowci = colDef(name = "Lower CI")
                 )
       )
     })
