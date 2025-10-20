6a6ef11ff1f3483ca2450143b5503784bf331ce4
monicamcgibbon <monica.mcgibbon@phs.scot>
2025-10-20 14:22:23 +0000

    demography domain spelling

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -112,7 +112,7 @@ profiles_list <- list(
   "Climate" = list(
     short_name = "CLI",
     homepage_description = markdown("View indicators relating to **Climate Health Impact**, **Weather**, **Air Quality**, **Climate Attitude** and **Demography**"),
-    domain_order = c("Climate Health Impact", "Weather", "Air Quality", "Climate Attitude", "Demography."),
+    domain_order = c("Climate Health Impact", "Weather", "Air Quality", "Climate Attitude", "Demography"),
     subtabs = c("trends_tab", "rank_tab", "pop_groups_tab", "about_profile_tab"),
     active = TRUE
   ),
