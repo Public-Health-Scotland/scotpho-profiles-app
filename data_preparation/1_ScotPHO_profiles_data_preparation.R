b779dda46ea4871856da74dfc51f925e70104e28
monicamcgibbon <monica.mcgibbon@phs.scot>
2025-12-01 13:47:25 +0000

    amend data prep script

--- data_preparation/1_ScotPHO_profiles_data_preparation.R
+++ data_preparation/1_ScotPHO_profiles_data_preparation.R
@@ -1,191 +1,265 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 1_ScotPHO_profiles_data_preparation.R
-# This script prepares the data behind the ScotPHO Profiles Tool
-#
-# There are a number of data files that profile tool app requires.
-# Updating the tool will require updating one or more of the following data files
-#
-# Meta data files
-# 1 - Technical document - excel file located within network directory holding all meta about indicators
-# 2 - Geography lookup - details linking geography codes to geography names plus heirarchy of which areas are within others
-# 3 - Shapefiles - used to generate maps within rank visualisations - updating these is rarely required
-#
-# Files containing indicator data (which parts of profiles tool depend on them)
-# 4 - main data (summary/rank/trend tabs)
-# 5 - SIMD data (SIMD/deprivation tab)
-# 6 - population groups data (population group tab)
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Data preparation script ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This script prepares 4 files required for the dashboard:
+# 3 indicator datasets and 1 metadata file
+
+# Note there are also 8 geography files required to run the dashboard
+# (one of which is needed to run this script)
+# if you are running this script for the first time OR if the geography lookups have been updated
+# then run the 'update geography lookups.R' script first to generate these files and save them in your project
 
 
-######################################.
-# Set up
-######################################.
+# If preparing data for deployment: - 
+# Change 'file_snapshot' arguments to TRUE
+# this will show you what files have been added/removed/updates since previous deployment
+# and ensures a snapshot of the folders current state is taken, ready for the next analyst
+# to compare when next deploying.
 
-## Load libraries -----
-library(dplyr) # data wrangling
-library(openxlsx) # for reading in technical document / converting excel dates
-library(readr) # for reading csv files
-library(data.table) # for quickly combining multiple files
-#library(scales) # for re-scaling measures
-library(nanoparquet) # for writing parquet files
-library(janitor) # for cleaning column names
-library(assertthat) # for data validation tests
-library(purrr) # for copying multiple files at once
-library(tidyr) # for pivot longer
-library(sf)
+# If wanting to include test indicators: - 
+# Change test_indicator arguments to TRUE
+# This will also read in data from any 'Test Shiny Data' folders
+
+# If only wanting to include data for a particular profile: - 
+# Change profile_filter to profiles 3-letter capitalised abbreviation
 
 
-## Set file-paths
-data_folder <- "/PHI_conf/ScotPHO/Profiles/Data/"
-lookups <- paste0(data_folder, "Lookups/") 
-shape_files <- paste0(data_folder, "Shapefiles/")
-shiny_files <- paste0(data_folder, "Shiny Data") #folder which contains indicator data files ready for inclusion in live profiles tool
-test_shiny_files <- paste0(data_folder, "Test Shiny Data") #folder which contains indicator data files under development and not considered ready for live tool
-backups <- paste0(data_folder, "Backups/") #area for saving copies of historic files linked to shiny app in case we mess up a file and need to roll back
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Dependencies -----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Load packages ----
+library(dplyr)
+library(data.table)
+library(janitor)
+library(openxlsx)
+library(nanoparquet)
+library(purrr) 
+library(cli)
+library(rlang)
+library(tools)
+library(utils)
 
 
-## Source function scripts
-source("data_preparation/2_dataprep_functions.R") # generic functions 
-source("data_preparation/2_dataprep_validation_tests.R") # validation checks
-
-source("data_preparation/update_techdoc.R") # script to read in & format techdoc
-source("data_preparation/update_shapefiles.R") # script to read in & format shapefiles
-source("data_preparation/update_deprivation_data.R") # script to read in & format deprivation data
-source("data_preparation/update_main_data.R") # script to read in and forma main data
-source("data_preparation/update_popgroup_data.R") # script to read in and forma main data
+## Source functions ----
+list.files("data_preparation/functions", full.names = TRUE) |> walk(~ source(.x))
 
 
-###################################################.
-## Update technical document ----
-##################################################.
+## Filepaths ----
 
-# Generates techdoc parquet file within shiny app data folder
-# Also makes technical_doc visible in data pane
-# which are required for processing the indicator data files below.
-# Option:  to save a backup version of techdoc (set create_backup to TRUE)
-# Option: to include indicators data labelled as test indicators in techdoc (set load test indicators to TRUE)
+# scotpho network folder:
+# bulk of metadata and indicator data sits in sub-folders within this folder
+# (for indicators managed by scotpho analysts)
+scotpho_folder <- "/PHI_conf/ScotPHO/Profiles/Data"
 
-update_techdoc(load_test_indicators = FALSE, create_backup = FALSE)
-
-# PLANNING ON UPDATING INDICATORS AND DEPLOYING THE APP? consider generating backup of techdoc. 
-# update_techdoc(load_test_indicators = FALSE, create_backup = TRUE)
+# collaboration folder:
+# subfolder in scotpho network folder where teams developing new indicators/profiles 
+# save their data/metadata (when they are responsible for updating going forward)
+collab_folder <- file.path(scotpho_folder, "Collaborations")
 
 
-###################################################.
-## Update geography lookups  ----
-###################################################.
+# collaboration sub-folders for specific teams/profiles:
+climate_folder <- file.path(collab_folder, "Climate")
 
-## Geography Lookup
-# copy the main geography lookup to your local repo
-file.copy(
-  paste0(lookups, "Geography/opt_geo_lookup.rds"), # old file path - TO DO rename opt_geo_lookup to profiles_geo_lookup in looksups repo
-  paste0("shiny_app/data/profiles_geo_lookup.rds"),  # copy to local shiny app data folder
-  overwrite = TRUE
+
+# project data folder:
+# a sub-folder WITHIN this scotpho-profiles-app R project
+# where the files generated in this script are saved
+# ready to be read into the shiny app via the 'Global.R' script
+project_folder <- file.path("shiny_app/data")
+
+
+# create project folder if doesn't already exist
+if (!dir.exists (project_folder)){
+  dir.create(project_folder)
+}
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 1 - Prepare metadata lookup ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Technical Documents (techdocs) contain metadata for each indicator in the app
+# This step reads in, formats and combines multiple techdocs to create one version for the app
+# This file is used in the indicator metadata panels under each tab of the app 
+
+
+# Read in scotpho's techdoc and other team/profile specific techdocs from collaboration sub-folders 
+techdocs_list <- list(
+  "scotpho" = prepare_techdoc(
+    filepath = file.path(scotpho_folder, "Lookups/Technical_Document.xlsx"), 
+    test_indicators = FALSE
+    ),
+  "climate" = prepare_techdoc(
+    filepath = file.path(climate_folder, "Lookups/Technical_Document.xlsx"), 
+    test_indicators = FALSE
+    )
+  )
+
+
+# combine techdocs to create one "master copy" of metadata 
+# for all indicators in the dashboard
+techdocs_combined <- list_rbind(techdocs_list)
+
+
+# check if same ind_id exists in more than 1 techdoc: ind_ids should be 
+# unique to each indicator, not just indicators within a specific techdocs!
+duplicate_inds <- techdocs_combined |>
+  group_by(ind_id) |>
+  filter(n() > 1)
+
+
+# remove columns not required
+techdocs_combined <- techdocs_combined |>
+  select(-c(supress_less_than, techdoc_path))
+
+
+# if no duplicates found then save the metadata in this projects
+# data folder ready to be used within the shiny app 
+if (nrow(duplicate_inds) == 0){
+  write_parquet(techdocs_combined, file = file.path(project_folder, "techdoc.parquet"))
+}
+
+
+# remove objects from global env.
+rm(techdocs_list, techdocs_combined, duplicate_inds)
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 2: Prepare main dataset ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This dataset is where the bulk of indicator data for the shiny app sits
+# Every indicator in the app will have some data within this dataset
+# Used across the 'summary', 'trend' and 'rank' tabs of the app
+
+
+# combine files ending in '_shiny.csv from 'Shiny Data' folder
+# within /PHI_conf/ScotPHO/Profiles/Data
+scotpho_main <- create_dataset(
+  dataset = "main", 
+  folder = scotpho_folder, 
+  file_suffix = "_shiny.csv", 
+  test_indicators = FALSE,
+  profile_filter = NULL,
+  file_snapshot = FALSE # set to TRUE if deploying
+  )
+
+
+# VE 2025: temporarily remove this indicator at HSC locality 
+# level as still uses old codes
+scotpho_main <- scotpho_main |>
+  filter(!(indicator == "Children in low income families" &
+             substr(code, 1, 3) == "S99"))
+
+
+
+# combine files ending in '_shiny.parquet' from 'Shiny Data'
+# folder within /PHI_conf/ScotPHO/Profiles/Data/Collaborations/Climate
+climate_main <- create_dataset(
+  dataset = "main",
+  folder = climate_folder,
+  file_suffix = "_shiny.parquet",
+  test_indicators = FALSE,
+  profile_filter = NULL,
+  file_snapshot = FALSE # set to TRUE if deploying
 )
 
-# open geography look from shiny app data folder 
-# (required for matches on areanames, area type and parent geographies to geography codes in indicator datasets)
-geography_lookup <- readRDS(
-  file = paste0(lookups, "Geography/opt_geo_lookup.rds") ##TO DO rename opt_geo_lookup to profiles_geo_lookup in looksups repo
+# combine all main datasets 
+main_dataset <- rbind(scotpho_main, climate_main)
+
+
+# save main dataset in this project data folder, ready to be used in shiny app
+write_parquet(main_dataset, file = file.path(project_folder, "main_dataset.parquet"), compression = "zstd")
+
+
+
+# clear global env.
+rm(list = ls(pattern = "main"))
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 3: Prepare popgroups dataset -----              
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This dataset only contains some indicators, where an extra file has been produced
+# for that indicator containing some population group splits (such as age, sex etc.)
+# Used for the 'population groups' tab of the app 
+
+
+# combine files ending in 'popgrp.csv from 'Shiny Data' folder
+# within /PHI_conf/ScotPHO/Profiles/Data
+scotpho_popgrp <- create_dataset(
+  dataset = "popgroup",
+  folder = scotpho_folder,
+  file_suffix = "popgrp.csv",
+  test_indicators = FALSE,
+  profile_filter = NULL,
+  file_snapshot = FALSE # set to TRUE if deploying
 )
 
 
 
-#########################################################################################.
-## Update main dataset  ----
-## information that populates summary/trend/rank tabs
-#########################################################################################.
-
-# switch to TRUE if including test indicators (note that you will also need to load test indicators in the update_techdoc function)
-# create_backup - switch to true if deploying the live app with updated indicator datasets 
-
-update_main_data(load_test_indicators = FALSE, create_backup = FALSE)
-
-# run validation tests one by one 
-# when a test is finished running, if it's passed 'TRUE' will print in the console
-# otherwise pay attention to the message in the console detailing why the test has failed
-TEST_no_missing_indicators(main_dataset) # compares indicators in dataset to active indicators in techdoc  
-TEST_no_missing_geography_info(main_dataset) # all rows have valid geography code
-TEST_no_missing_metadata(main_dataset) # checks for indicators with missing metdata (i.e. if failed to join with techdoc)
-TEST_suppression_applied(main_dataset) # double checking suppression function wasn't skipped
+# combine files ending in 'popgrp.parquet' from 'Shiny Data'
+# folder within /PHI_conf/ScotPHO/Profiles/Data/Collaborations/Climate
+climate_popgrp <- create_dataset(
+  dataset = "popgroup",
+  folder = climate_folder,
+  file_suffix = "popgrp.parquet",
+  test_indicators = FALSE,
+  profile_filter = NULL,
+  file_snapshot = FALSE # set to true if deploying
+)
 
 
+# combine all popgroup datasets
+popgrp_dataset <- rbind(scotpho_popgrp, climate_popgrp)
 
 
-#######################################################################################.
-## Create geography nodes file 
-#######################################################################################.
+# save popgroup dataset in this projects data folder, ready to be used in the app
+write_parquet(popgrp_dataset, file = file.path(project_folder, "popgroup_dataset.parquet"), compression = "zstd")
 
 
-# get a distinct list of geography paths in the main dataset
-main_dataset_geography_list <- main_dataset |>
-  select(geo_path) |>
-  distinct()
-
-main_dataset <- main_dataset |>
-  mutate(areatype = factor(areatype, levels = c("Scotland", 
-                                                "Council area", 
-                                                "Health board",
-                                                "Alcohol & drug partnership",
-                                                "HSC partnership",
-                                                "HSC locality",
-                                                "Intermediate zone",
-                                                "Police division"))) |>
-  arrange(areatype, parent_area, areaname)
-
-# convert them into lists of parent/child nodes that can be used to create a 
-# hierarchical geography filter in the data tab of the shiny app
-main_dataset_geography_list <- create_geography_nodes(main_dataset_geography_list$geo_path)
+# clear global env.
+rm(list = ls(pattern = "popgrp"))
 
 
-# save file to be used in app
-saveRDS(main_dataset_geography_list, "shiny_app/data/main_dataset_geography_nodes.rds")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 4: Prepare deprivation dataset -----              
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This dataset only contains some indicators, where an extra file has been produced
+# for that indicator containing inequalities splits (i.e. SIMD, RII, SII, PAR)
+# Used for the 'Deprivation' tab of the app 
+
+# combine files ending in 'ineq.rds' from 'Shiny Data' folder
+# within /PHI_conf/ScotPHO/Profiles/Data
+depr_dataset <- create_dataset(
+  dataset = "deprivation",
+  folder = scotpho_folder,
+  file_suffix = "ineq.rds",
+  test_indicators = FALSE,
+  profile_filter = NULL,
+  file_snapshot = FALSE # set to true if deploying
+)
+
+# MM Nov 2025 - temporarily remove scotland decile data
+# as we currently only publish quintiles
+depr_dataset <- depr_dataset |>
+  filter(quint_type != "sc_decile")
 
 
-###############################################################.
-## Update deprivation data  ----
-## i.e. indicator data split by SIMD quintiles.
-##############################################################.
-
-update_deprivation_data(load_test_indicators = FALSE, create_backup = FALSE)
-
-## Decide which fields actually need to be fed into profiles tool - some are required for validation checks but not sure these are needed in app or have different names.
-
-# run validation tests
-# TEST_no_missing_ineq_indicators(deprivation_dataset) # compares dataset to techdoc column 'inequality label' is not null 
-TEST_no_missing_geography_info(deprivation_dataset) # all rows have valid geography code
-TEST_no_missing_metadata(deprivation_dataset) # checks for dep indicators with no indicator name
-TEST_suppression_applied(deprivation_dataset) # double checking suppression function wasn't skipped
-# TEST_inequalities_trends(deprivation_dataset) # checks if last deprivation indicator year is same as main profiles dataset max year (wont run until main data also in data prep)
+# save deprivation dataset in this project data folder, ready to be used in the app
+write_parquet(depr_dataset, file = file.path(project_folder, "deprivation_dataset.parquet"), compression = "zstd")
 
 
 
-########################################################################.
-## Update population groups data  ----
-## i.e. indicator data split by various different inequality groups.
-########################################################################.
-
-update_popgroup_data(load_test_indicators = FALSE, create_backup = FALSE)
-
-# no validation tests currently written for population data.
-
-
-############################################################.
-## Update shape files  ----
-############################################################.
-
-# Shapefiles (used to generate maps presented in rank tab only)
-# Note: this step is only really necessary if you are running this script for the first time
-# OR if there have been updates to the shape files
-update_shapefiles()
-
-
-###############################################################.
-## Remove objects  ----
-###############################################################.
-
-# remove objects from global environment before running shiny app 
+# clear global env.
 rm(list = ls())
 
-##END
+
+
