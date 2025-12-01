a3c2c292159f9c1ca1132410b3baf318490ff8f9
monicamcgibbon <monica.mcgibbon@phs.scot>
2025-12-01 13:47:53 +0000

    add geography lookup files prep to it's own script

---
+++ data_preparation/update geography lookups.R
@@ -0,0 +1,180 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Update geography lookups ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This script prepares 8 x files required to run the profiles tool
+# 2 x geography lookups and 6 x shapefiles
+
+# It will save the files in a 'data' sub-folder WITHIN this project
+# ready to be read in via the 'Global.R' script of the shiny app
+
+# Only need to run this script if running the app for the first time
+# OR if there have been updates to any of these files (e.g. locality boundary changes etc.)
+
+
+## filepaths ----
+
+# sub-folder where lookups will be saved to
+project_folder <- "shiny_app/data"
+
+# create folder if doesn't already exists
+if (!dir.exists(project_folder)){
+  dir.create(project_folder)
+}
+
+# scotpho network area folder 
+scotpho_folder <- "/PHI_conf/ScotPHO/Profiles/Data"
+
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1. geography lookup ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# This is the apps main geography lookup and is used
+# to generate choices for any geography filters within the app
+# (i.e. filters on the 'trend' tab and the apps main geography filters)
+
+# It's also used within the data preparation script to 
+# join with indicator datasets in order to add extra geography columns to them
+
+# get lookup 
+geo_lookup <- readRDS(file.path(scotpho_folder, "Lookups/Geography/opt_geo_lookup.rds")) |>
+  select(-areaname_full)
+
+
+# arrange areatypes
+geo_lookup <- geo_lookup |>
+  mutate(areatype = factor(
+    areatype,
+    levels = c(
+      "Scotland",
+      "Health board",
+      "Council area",
+      "Alcohol & drug partnership",
+      "HSC partnership",
+      "HSC locality",
+      "Intermediate zone",
+      "Police division"
+    )
+  )) |>
+  arrange(areatype, parent_area, areaname)
+
+
+
+# add additional column to the lookup that 
+# contains the full geography path (e.g. Health board/NHS Ayrshire & Arran)
+geo_lookup <- geo_lookup |>
+  mutate(
+    geo_path = paste(
+      areatype,
+      case_when(
+        areatype %in% c("Intermediate zone", "HSC locality") ~ parent_area,
+        TRUE ~ areaname
+      ),
+      case_when(
+        areatype %in% c("Intermediate zone", "HSC locality") ~ areaname,
+        TRUE ~ NA_character_
+      ),
+      sep = "/"
+    ),
+    geo_path = sub("/NA$", "", geo_path)
+  )
+
+
+saveRDS(geo_lookup, file.path(project_folder, "profiles_geo_lookup.rds"))
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. geography list -----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# This lookup is used only on the data table tab of the app, in order
+# to create the heirarchical geography filter in the sidebar.
+# this code is lifted from the documentation for the 'jsTreeR' package 
+# (which is the package used to create the geography filter in the data table tab)
+# see examples here: https://www.rdocumentation.org/packages/jsTreeR/versions/1.1.0/topics/jstree-shiny 
+
+
+# get unique geographies from geo lookup
+leaves <- geo_lookup$geo_path 
+
+
+dfs <- lapply(strsplit(leaves, "/"), function(s){
+  item <-Reduce(function(a,b) paste0(a,"/",b), s[-1], s[1], accumulate = TRUE)
+  data.frame(
+    item = item,
+    parent = c("root", item[-length(item)]),
+    stringsAsFactors = FALSE
+  )
+})
+
+
+#dat <- dfs[[1]]
+# for(i in 2:length(dfs)){
+#   dat <- merge(dat, dfs[[i]], all = TRUE)
+# }
+
+# amending function using 2 lines below to use rbind instead of merge 
+# This allows parent nodes to be ordered based on how data is arranged before being passed to this function
+# instead of alphabetically
+dat <- do.call(rbind, dfs)
+dat <- dat[!duplicated(dat), ]
+
+f <- function(parent){
+  i <- match(parent, dat$item)
+  item <- dat$item[i]
+  children <- dat$item[dat$parent==item]
+  label <- tail(strsplit(item, "/")[[1]], 1)
+  if(length(children)){
+    list(
+      text = label,
+      children = lapply(children, f),
+      icon =FALSE,
+      state = list(selected = FALSE, opened = FALSE )
+    )
+  }else{
+    list(text = label, type = "child",icon = FALSE,
+         state = list(selected = FALSE,opened = FALSE ))
+  }
+}
+
+geo_list <- lapply(dat$item[dat$parent == "root"], f)
+
+saveRDS(geo_list, file.path(project_folder, "main_dataset_geography_nodes.rds"))
+
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3. shapefiles -----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# These shapefiles are used to draw the maps on the 'rank' tab of the app
+
+shapefiles <- c("CA_boundary.rds", 
+                "HB_boundary.rds", 
+                "HSCP_boundary.rds",
+                "HSC_locality_boundary.rds",
+                "IZ_boundary.rds",
+                "PD_boundary.rds")
+
+
+
+purrr::walk(shapefiles, ~ {
+  readRDS(file.path(scotpho_folder, "Shapefiles", .x)) |>
+    sf::st_as_sf() |>
+    readr::write_rds(file.path(project_folder, .x))
+  cli_alert_success("{.val {.x}} saved in `shiny_app/data` folder")
+})
+
+
+
+
+# clear global env
+rm(list = ls())
