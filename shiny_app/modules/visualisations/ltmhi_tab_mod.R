204b122cad3109f44b015cf926fc0a1b9455b226
monicamcgibbon <monica.mcgibbon@phs.scot>
2026-02-03 08:07:07 +0000

    add summary table

--- shiny_app/modules/visualisations/ltmhi_tab_mod.R
+++ shiny_app/modules/visualisations/ltmhi_tab_mod.R
@@ -30,7 +30,7 @@ ltmhi_UI <- function(id, ltmhi_dataset) {
       nav_panel(
         title = "Summary table",
         class = "container",
-        p("placeholder")
+        card(reactableOutput(ns("summary_tbl")))
       ),
 
 
@@ -38,7 +38,12 @@ ltmhi_UI <- function(id, ltmhi_dataset) {
       nav_panel(
         title = "Explore indicators",
         class = "container",
-        p("placeholder")
+        # indicator filter 
+        selectizeInput(
+          inputId = ns("ind_filter"),
+          label = NULL,
+          choices = split(ltmhi_info$domain, ltmhi_info$indicator)
+        )
       ),
 
 
@@ -66,7 +71,171 @@ ltmhi_Server <- function(id) {
     id,
     function(input, output, session) {
       
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Summary table server code ----
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      
+      
+      # icon to use depending on direction of SII/RII trend 
+      direction_icon <- function(value) {
+        switch(
+          value, 
+          "Widening" =  bsicons::bs_icon("arrow-up", size = "1.5em", class = "text-red"),
+          "Narrowing" = bsicons::bs_icon("arrow-down", size = "1.5em", class = "text-green"),
+          "Unchanged" = bsicons::bs_icon("dash", size = "1.5em", class= "text-black")
+        )
+      }
+      
+
+      
+      
+      # render the summary table 
+      output$summary_tbl <- renderReactable({
+        
+        reactable(
+          data = ltmhi_lookup,
+          highlight = TRUE,
+          pagination = FALSE,
+          columns = list(
+            
+            # hidden columns
+            sii_trend_period = colDef(show = FALSE),
+            rii_trend_period = colDef(show = FALSE),
+            comparison_period = colDef(show = FALSE),
+            description = colDef(show = FALSE),
+            
+            
+            # domain column
+            domain = colDef(
+              name = "Domain",
+              cell = function(value, index) {
+                if (index > 1 && shi_lookup$domain[index] == shi_lookup$domain[index - 1]) {
+                  div(style = "visibility:hidden", value)
+                } else value
+              }
+            ),
+            
+            
+            # comparison column
+            comparison = colDef(
+              name = "Most vs. least deprived quintile",
+              html = TRUE,
+              cell = function(value, index){
+                as.character(tooltip(
+                  div(class = "badge rounded-pill ms-auto", style = "background-color: #adb5bd", value),
+                  paste(value, "in the least deprived quintile in", shi_lookup$comparison_period[index])
+                ))
+              }
+            ),
+            
+            
+            # indicator column
+            indicator = colDef(
+              name = "Indicator",
+              minWidth = 270,
+              html = TRUE,
+              cell = function(value){
+                tags$a(
+                  href = "#section-3",
+                  value,
+                  # when link is clicked, update input$indicator_clicked with the name of the indicator
+                  # which will trigger an observeEvent() that is spying on this input run in order to
+                  # go to  'explore indicators' section and select indicator from filter
+                  onclick = sprintf(
+                    "Shiny.setInputValue('%s', %s, {priority:'event'});",
+                    input$indicator_clicked,
+                    jsonlite::toJSON(value, auto_unbox = TRUE)
+                  )
+                  
+                )
+              }
+            ),
+            
+            
+            # absolute inequalities direction column
+            sii_trend = colDef(
+              align = 'center',
+              html = TRUE,
+              name = "Absolute Inequalities Trend",
+              cell = function(value, index){
+                as.character(tooltip(
+                  direction_icon(value),
+                  paste(value, "since", shi_lookup$rii_trend_period[index])
+                ))
+              }
+            ),
+            
+            # relative inequalities direction column
+            rii_trend = colDef(
+              align = 'center',
+              html = TRUE,
+              name = "Relative Inequalities Trend",
+              cell = function(value, index){
+                as.character(tooltip(
+                  direction_icon(value),
+                  paste(value, "since", shi_lookup$rii_trend_period[index])
+                ))
+              }
+            ),
+            
+            
+            # Overall trend column
+            overall_trend = colDef(
+              align = 'center',
+              name = "Overall Direction of Inequalities",
+              cell = function(value){
+                
+                colour <- switch(
+                  value,
+                  "Widening" = "red",
+                  "Narrowing" = "green",
+                  "Unchanged" = "black"
+                )
+                
+                div(class = paste0("badge rounded-pill ms-auto bg-", colour), value)
+              }
+            )
+          )
+        )
+      })
+      
+      
+      
+      # update selection from indicator filter in 'explore indicators' section 
+      # whenever link clicked in summary table (i.e. when input$indicator_clicked changes)
+      observeEvent(input$indicator_clicked, {
+        updateSelectizeInput(
+          session = session,
+          inputId = "ind_filter",
+          selected = input$indicator_clicked
+        )
+      })
+      
+      
+      
+      
     }
   )
 }
+
+
+
+
+
+# For testing:
+
+
+# shinyApp(
+#   ui = page_navbar(
+#     nav_panel(
+#       title = "National profile",
+#       ltmhi_UI("example")
+#       )
+#     ),
+#   server = function(input, output, session){
+#     ltmhi_Server("example")
+#   }
+# )
+
+
 
