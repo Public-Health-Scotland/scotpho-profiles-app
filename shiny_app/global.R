3345dd45b06938836daf4b90912d9657976e8204
abigap01 <abigail.prescott@phs.scot>
2026-03-06 15:40:22 +0000

    PA Profile Setup

--- shiny_app/global.R
+++ shiny_app/global.R
@@ -210,12 +210,12 @@ profiles_list <- list(
   # Physical Activity info
   "Physical Activity" = list(
     short_name = "PHY",
-    homepage_description = markdown("Under development - not yet available"),
+    homepage_description = markdown("View indicators related to **Active Places & Spaces**, **Active Places of Learning**,  **Active Travel** and **Sport & Active Recreation**"),
     domain_order = NULL,
-    subtabs = c("trends_tab", "rank_tab", "simd_tab"),
+    subtabs = c("summary_tab", "trends_tab", "rank_tab", "simd_tab", "pop_groups_tab"),
     nav_id = "Profiles",
-    new = FALSE,
-    active = FALSE
+    new = TRUE,
+    active = TRUE
   ),
   
   # All indicators info
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -85,6 +85,16 @@ page_navbar(
                           # hr(), 
                           
                           # Updates
+                          #Physical Activity Profile
+                          div(h4("March 2026: New Physical Activity profile", class = "profile-header"),
+                              p("The Physical Activity Profile, produced in collaboration with the University of Edinburgh, contains council- and health board-level indicators which report on
+                               active travel, uptake of the Chief Medical Officer's",
+                                tags$a("Physical Activity Guidelines,", href = "https://www.gov.uk/government/publications/physical-activity-guidelines-uk-chief-medical-officers-report",
+                                       target = "_blank")," Active Places & Spaces, Active Places of Learning and Sport & Active Leisure.")
+                          ),
+                          hr(),
+                          
+                          #Indicator Updates
                           div(
                             h4("February 2026 : Indicator updates", class = "profile-header"),
                             p("Our rolling indicator updates process is currently underway for data covering the period 2024 and 2024/2025.  Delays in the publication of small area (intermediate zone, HSCP locality) population denominators
@@ -110,16 +120,16 @@ page_navbar(
                          ),
                          hr(),
                                             
-                          # CYP Mental Health profile info
-                          div(
-                            h4("April 2025 : New Mental Health Profile for Children & Young People", class = "profile-header"),
-                            p("The Mental Health Profile for Children & Young People is the result of Public Health Scotland's", 
-                              tags$a("Mental Health Indicators project.", href = "https://publichealthscotland.scot/our-areas-of-work/health-and-wellbeing/prevention-of-mental-ill-health-and-improved-wellbeing/mental-health-indicators/overview/", 
-                              target = "_blank")," The mental health indicators include measures of mental health outcomes, as well as of a wide range 
-                              of interconnected determinants (risk factors and protective factors) of these outcomes. This profile follows ScotPHO's publication of the adult mental health profile in late 2024.")
-                          ),
-                          hr(),
-                  
+                          # # CYP Mental Health profile info
+                          # div(
+                          #   h4("April 2025 : New Mental Health Profile for Children & Young People", class = "profile-header"),
+                          #   p("The Mental Health Profile for Children & Young People is the result of Public Health Scotland's", 
+                          #     tags$a("Mental Health Indicators project.", href = "https://publichealthscotland.scot/our-areas-of-work/health-and-wellbeing/prevention-of-mental-ill-health-and-improved-wellbeing/mental-health-indicators/overview/", 
+                          #     target = "_blank")," The mental health indicators include measures of mental health outcomes, as well as of a wide range 
+                          #     of interconnected determinants (risk factors and protective factors) of these outcomes. This profile follows ScotPHO's publication of the adult mental health profile in late 2024.")
+                          # ),
+                          # hr(),
+                          # 
               #   # Profiles tool re-design info : SEPT 2025 hide text since the 'new' design w as launched a year ago - keep script as a template for future notes
               #   div(
               #     h4("Profiles Tool Redesign", class = "profile-header"),
@@ -131,15 +141,15 @@ page_navbar(
               #   ),
               # hr(),
               # Adult Mental Health profile info
-              div(
-                h4("October 2024 : New Mental Health Profile for Adults", class = "profile-header"),
-                p("The Mental Health Profile for Adults is the result of Public Health Scotland's", tags$a("Mental Health Indicators project.", 
-                  href = "https://publichealthscotland.scot/our-areas-of-work/health-and-wellbeing/prevention-of-mental-ill-health-and-improved-wellbeing/mental-health-indicators/overview/", 
-                  target = "_blank")," The mental health indicators include measures of mental health outcomes, as well as of a wide range 
-                  of interconnected determinants (risk factors and protective factors) of these outcomes. ScotPHO have 
-                 recently published this profile for indicators in adults and are currently working on the profile for children and young people.")
-                ),
-              hr(),
+              # div(
+              #   h4("October 2024 : New Mental Health Profile for Adults", class = "profile-header"),
+              #   p("The Mental Health Profile for Adults is the result of Public Health Scotland's", tags$a("Mental Health Indicators project.", 
+              #     href = "https://publichealthscotland.scot/our-areas-of-work/health-and-wellbeing/prevention-of-mental-ill-health-and-improved-wellbeing/mental-health-indicators/overview/", 
+              #     target = "_blank")," The mental health indicators include measures of mental health outcomes, as well as of a wide range 
+              #     of interconnected determinants (risk factors and protective factors) of these outcomes. ScotPHO have 
+              #    recently published this profile for indicators in adults and are currently working on the profile for children and young people.")
+              #   ),
+              # hr(),
               # Feedback info
               div(
                 h4("Feedback", class = "profile-header"),
